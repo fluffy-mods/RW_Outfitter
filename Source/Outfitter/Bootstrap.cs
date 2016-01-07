8f721fc2756860645ebe01e80d993ecafd6ac6eb
Karel Kroeze <karel.kroeze@gmail.com>
2016-01-07 11:47:08 +0100

    load Itab into all humanlikes attempt to force EdB interface to reload itabs

--- Source/Outfitter/Bootstrap.cs
+++ Source/Outfitter/Bootstrap.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using CommunityCoreLibrary;
+using EdB.Interface;
 using RimWorld;
 using Verse;
 
@@ -25,21 +27,36 @@ namespace Outfitter
     {
         public override void Inject()
         {
-            // inject ITab
-            // TODO: Inject into other humanlike pawns.
-            ThingDef def = ThingDef.Named( "Human" );
-            if( def.inspectorTabs == null || def.inspectorTabs.Count == 0 )
+            // inject ITab into all humanlikes
+            foreach ( ThingDef def in DefDatabase<ThingDef>.AllDefsListForReading.Where( td => td.category == ThingCategory.Pawn && td.race.Humanlike ) )
             {
-                def.inspectorTabs = new List<Type>();
-                def.inspectorTabsResolved = new List<ITab>();
-            }
-            if( def.inspectorTabs.Contains( typeof( ITab_Pawn_Outfitter ) ) )
-            {
-                return;
+                if( def.inspectorTabs == null || def.inspectorTabs.Count == 0 )
+                {
+                    def.inspectorTabs = new List<Type>();
+                    def.inspectorTabsResolved = new List<ITab>();
+                }
+                if( def.inspectorTabs.Contains( typeof( ITab_Pawn_Outfitter ) ) )
+                {
+                    return;
+                }
+
+                def.inspectorTabs.Add( typeof( ITab_Pawn_Outfitter ) );
+                def.inspectorTabsResolved.Add( ITabManager.GetSharedInstance( typeof( ITab_Pawn_Outfitter ) ) );
             }
 
-            def.inspectorTabs.Add( typeof( ITab_Pawn_Outfitter ) );
-            def.inspectorTabsResolved.Add( ITabManager.GetSharedInstance( typeof( ITab_Pawn_Outfitter ) ) );
+            // have EdB reload the tabs
+            // There appears to be a race condition between EdB tab replacements and our extra tab. If EdB goes first, ours does not get shown until EdB refreshes tabs.
+            // TODO: Figure out how to get to the ComponentTabReplacement to call the reset there
+            foreach ( IPreference preference in Preferences.Instance.Groups.SelectMany( group => group.Preferences ) )
+            {
+                if ( preference is PreferenceTabArt )
+                {
+                    // insanely silly on off toggle just to get EdB to set the dirty toggle and reload ITabs so our custom one should always get loaded.
+                    ( (PreferenceTabArt)preference ).Value = !( (PreferenceTabArt)preference ).Value;
+                    ( (PreferenceTabArt)preference ).Value = !( (PreferenceTabArt)preference ).Value;
+                    break;
+                }
+            }
         }
     }
 }
